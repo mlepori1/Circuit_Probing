151261073daf1f8d0921e37ebaa75a2d8821a70f
Michael Lepori <mlepori@vscode1.oscar.ccv.brown.edu>
2023-06-19 19:48:10 -0400

    deleted transformers repo

--- src/ProbeDataset.py
+++ src/ProbeDataset.py
@@ -1,16 +1,17 @@
 from torch.utils.data import Dataset
 import torch
 import random
-from data_utils import read_conll_format, read_onto_notes_format
+from amnesic_probing.amnesic_probing.encoders import read_conll_format
+from transformers import BertTokenizerFast
 
 class ProbeDataset(Dataset):
 
     def __init__(self, path, label, tokenizer, seed=0):
         random.seed(seed)
         
-        if ".conllu" in path:
+        if ".conll" in path:
             data = read_conll_format(path)
-            label2id = self.create_label2id(data, label)
+            self.label2id = self.create_label2id(data, label)
 
         random.shuffle(data)
                       
@@ -18,20 +19,23 @@ class ProbeDataset(Dataset):
         self.x = []
         self.y = []
         for d in data:
-            self.x.append(" ".join(d["text"]))
-            self.y.append([label2id.index(lab) for lab in d["labels"][label]])
+            self.x.append(d["text"])
+            self.y.append([self.label2id.index(lab) for lab in d["labels"][label]])
 
     def create_label2id(self, data, label):
         label2id = set()
         for d in data:
             label2id.update(d["labels"][label])
-        return list(label2id)
+        label2id = list(label2id)
+        label2id.sort()
+        return label2id
 
     def __len__(self):
         return len(self.x)
 
     def __getitem__(self, idx):
-        x = self.tokenizer([self.x[idx]])
+        print(self.x[idx])
+        x = self.tokenizer([self.x[idx]], is_split_into_words=True, return_tensors="pt")
         y = self.y[idx]
         sample = {
             'input_ids': x["input_ids"][0], 
@@ -39,3 +43,11 @@ class ProbeDataset(Dataset):
             'token_type_ids': x["token_type_ids"][0], 
             'label': y}
         return sample
+
+
+if __name__=="__main__":
+    tok = BertTokenizerFast.from_pretrained("bert-base-uncased")
+    dataset = ProbeDataset("../data/UD/en-universal-train.conll", "tag", tok, seed=1)
+    print(dataset[0])
+    print(len(dataset.label2id))
+    print(len(dataset))
